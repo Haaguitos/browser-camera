beb302941755d5f2a5c9b0a0109179685e42b069
Ígor Haag <igorhaagrod@gmail.com>
2023-12-13 22:13:22 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,124 @@
-import Image from 'next/image'
+"use client";
+
+import { useEffect } from "react";
+
+type WindowDetails = {
+  screenX: number;
+  screenY: number;
+  screenWidth: number;
+  screenHeight: number;
+  width: number;
+  height: number;
+  updated: number;
+};
 
 export default function Home() {
+  useEffect(() => {
+    const stats = document.querySelector<HTMLPreElement>(".stats");
+    const clear = document.querySelector<HTMLButtonElement>(".clear");
+    const video = document.querySelector<HTMLVideoElement>("video");
+
+    function getScreens(): [string, WindowDetails][] {
+      return Object.entries(window.localStorage)
+        .filter(([key]) => key.startsWith("screen-"))
+        .map(([key, value]: [string, string]) => [
+          key,
+          JSON.parse(value) as WindowDetails,
+        ]);
+    }
+
+    function getScreenId() {
+      const existingScreens = Object.keys(window.localStorage)
+        .filter((key) => key.startsWith("screen-"))
+        .map((key) => parseInt(key.replace("screen-", "")))
+        .sort((a, b) => a - b);
+      return existingScreens.at(-1) + 1 || 1;
+    }
+
+    const screenId = `screen-${getScreenId()}`;
+
+    function setScreenDetails() {
+      const windowDetails = {
+        screenX: window.screenX,
+        screenY: window.screenY,
+        screenWidth: window.screen.availWidth,
+        screenHeight: window.screen.availHeight,
+        width: window.outerWidth,
+        height: window.innerHeight,
+        updated: Date.now(),
+      };
+      window.localStorage.setItem(screenId, JSON.stringify(windowDetails));
+      // console.log(windowDetails);
+    }
+
+    function displayStats() {
+      if (!stats) return;
+      const existingScreens = Object.fromEntries(getScreens());
+      console.log("existingScreens", existingScreens);
+      stats.innerHTML = JSON.stringify(existingScreens, null, " ");
+    }
+
+    function restart() {
+      console.log(timers);
+      timers.forEach((timer) => window.clearInterval(timer));
+      window.localStorage.clear();
+      setTimeout(() => window.location.reload(), Math.random() * 1000);
+    }
+
+    function removeScreen() {
+      console.log(`removing screen ${screenId}`);
+      localStorage.removeItem(screenId);
+    }
+
+    function removeOld() {
+      const screens = getScreens();
+      for (const [key, screen] of screens) {
+        if (Date.now() - screen.updated > 1000) {
+          localStorage.removeItem(key);
+        }
+      }
+    }
+
+    function setVideo() {
+      video?.setAttribute(
+        "style",
+        `transform: translate(-${window.screenX}px, -${window.screenY}px)`
+      );
+    }
+
+    const timers: ReturnType<typeof setInterval>[] = [];
+    function go() {
+      timers.push(setInterval(setScreenDetails, 10));
+      timers.push(setInterval(displayStats, 10));
+      timers.push(setInterval(removeOld, 100));
+      timers.push(setInterval(setVideo, 10));
+    }
+
+    clear?.addEventListener("click", restart);
+    window.addEventListener("beforeunload", removeScreen);
+
+    function populateWebcam() {
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        if (!video) return;
+        video.width = window.screen.availWidth;
+        video.height = window.screen.availHeight;
+        video.srcObject = stream;
+        video.play();
+      });
+    }
+
+    go();
+
+    populateWebcam();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <video src=""></video>
+      <div id="app">
+        <button className="clear">Clear</button>
+        <pre className="stats"></pre>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
